3a5c9bfecbdfd5cc51422c174140f049f8d7d9d2
SeberyDev <sebasmanager06@gmail.com>
2022-09-08 18:43:44 -0500

    Se agrego nodemon

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Iniciar Sesión" });
+});
+
+router.get("/dashboard", function (req, res, next) {
+  res.render("dashboard", { title: "Dashboard" });
 });
 
 module.exports = router;
